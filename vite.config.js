ad69070194a7b9a416e4db350379c63e78419769
Anthony bell <beyondthefold1989@gmail.com>
2025-12-09 13:28:27 -0500

    Add GitHub Pages deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/nativebridge-desktop/", // <-- REQUIRED for GitHub Pages
+  base: "/nativebridge-desktop/", 
 });
